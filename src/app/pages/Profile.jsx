f4bebe15b75512f4768506881d1bc13e22164a2f
rory <35566455+Thorekt@users.noreply.github.com>
2023-04-10 17:07:27 +0200

    Découpage composant page Profile

--- src/app/pages/Profile.jsx
+++ src/app/pages/Profile.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Account from '../components/Account';
 
 export default function Profile() {
   return (
@@ -12,36 +13,9 @@ export default function Profile() {
         <button className='edit-button'>Edit Name</button>
       </div>
       <h2 className='sr-only'>Accounts</h2>
-      <section className='account'>
-        <div className='account-content-wrapper'>
-          <h3 className='account-title'>Argent Bank Checking (x8349)</h3>
-          <p className='account-amount'>$2,082.79</p>
-          <p className='account-amount-description'>Available Balance</p>
-        </div>
-        <div className='account-content-wrapper cta'>
-          <button className='transaction-button'>View transactions</button>
-        </div>
-      </section>
-      <section className='account'>
-        <div className='account-content-wrapper'>
-          <h3 className='account-title'>Argent Bank Savings (x6712)</h3>
-          <p className='account-amount'>$10,928.42</p>
-          <p className='account-amount-description'>Available Balance</p>
-        </div>
-        <div className='account-content-wrapper cta'>
-          <button className='transaction-button'>View transactions</button>
-        </div>
-      </section>
-      <section className='account'>
-        <div className='account-content-wrapper'>
-          <h3 className='account-title'>Argent Bank Credit Card (x8349)</h3>
-          <p className='account-amount'>$184.30</p>
-          <p className='account-amount-description'>Current Balance</p>
-        </div>
-        <div className='account-content-wrapper cta'>
-          <button className='transaction-button'>View transactions</button>
-        </div>
-      </section>
+      <Account title='Argent Bank Checking (x8349)' amount='$2,082.79' />
+      <Account title='Argent Bank Savings (x6712)' amount='$10,928.42' />
+      <Account title='Argent Bank Credit Card (x8349)' amount='$184.30' />
     </main>
   );
 }
