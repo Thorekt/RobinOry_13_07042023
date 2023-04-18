058a48a89acdf4af3d20363fa30d60f760bf3625
rory <35566455+Thorekt@users.noreply.github.com>
2023-04-19 00:36:17 +0200

    selector for auth and profile

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
+import profileReducer from './features/profile';
+import authReducer from './features/auth';
 import { configureStore } from '@reduxjs/toolkit';
-
 
 export const store = configureStore({
   reducer: {
-    
+    auth: authReducer,
+    profile: profileReducer,
   },
 });
