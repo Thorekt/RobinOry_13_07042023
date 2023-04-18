468c98bc5ee942d8f6756718fe21c5d5ac6d01c4
rory <35566455+Thorekt@users.noreply.github.com>
2023-04-19 00:35:52 +0200

    remove code for react query

--- src/index.js
+++ src/index.js
@@ -3,18 +3,14 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
-import { QueryClient, QueryClientProvider } from 'react-query';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-const queryClient = new QueryClient();
 
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </QueryClientProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
