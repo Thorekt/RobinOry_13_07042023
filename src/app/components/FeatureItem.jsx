31a9e70f0c4347b297f3e652b6365cfbfc002e88
Robin Ory <35566455+Thorekt@users.noreply.github.com>
2023-05-07 16:41:46 +0200

    remplacement des featureitem par un parcours de list

--- src/app/components/FeatureItem.jsx
+++ src/app/components/FeatureItem.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function FeatureItem({ icon, title, text }) {
+export default function FeatureItem({ icon, title, text, alt, index  }) {
   return (
-    <div className='feature-item'>
-      <img src={icon} alt='Chat Icon' className='feature-icon' />
+    <div key={index} className='feature-item'>
+      <img src={icon} alt={alt} className='feature-icon' />
       <h3 className='feature-item-title'>{title}</h3>
       <p>{text}</p>
     </div>
--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -4,7 +4,34 @@ import iconMoney from '../img/icon-money.png';
 import iconSecurity from '../img/icon-security.png';
 import FeatureItem from '../components/FeatureItem';
 
+const features = [
+  {
+    icon: iconChat,
+    title: 'You are our #1 priority',
+    text:
+      'Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.',
+    alt: 'Chat Icon',
+  },
+  {
+    icon: iconMoney,
+    title: 'More savings means higher rates',
+    text:
+      'The more you save with us, the higher your interest rate will be!',
+    alt: 'Money Icon',
+  },
+  {
+    icon: iconSecurity,
+    title: 'Security you can trust',
+    text:
+      'We use top of the line encryption to make sure your data and money is always safe.',
+    alt: 'Security Icon',
+  },
+];
+
 export default function Home() {
+
+
+
   return (
     <main>
       <div className='hero'>
@@ -18,21 +45,15 @@ export default function Home() {
       </div>
       <section className='features'>
         <h2 className='sr-only'>Features</h2>
-        <FeatureItem
-          icon={iconChat}
-          title='You are our #1 priority'
-          text='Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.'
-        />
-        <FeatureItem
-          icon={iconMoney}
-          title='The more you save with us, the higher your interest rate will be!The more you save with us, the higher your interest rate will be!'
-        />
-        <FeatureItem
-          icon={iconSecurity}
-          title='Security you can trust'
-          text='We use top of the line encryption to make sure your data and money
-          is always safe.'
-        />
+        { features.map((feature, index) => (
+            <FeatureItem
+            icon={feature.icon}
+            title={feature.title}
+            text={feature.text}
+            alt={feature.alt}
+            key={index}
+          />
+        ))}
       </section>
     </main>
   );
